daf3c7296032c16d36537939bf925d8b97f35c3a
w2starry <2022575580@qq.com>
2024-05-30 02:25:59 +0800

    try delete preprocess

--- liasece_sd_webui_train_tools/dateset_ui.py
+++ liasece_sd_webui_train_tools/dateset_ui.py
@@ -102,35 +102,7 @@ def on_ui_update_dataset_click(id: str, project: str, version: str, input_train_
     os.makedirs(processed_output_path, exist_ok=True)
     os.makedirs(origin_preload_data_path, exist_ok=True)
 
-    try:
-        no_alpha_0_picture.rangeAllImage(origin_data_path, origin_preload_data_path)
-        preprocess.preprocess(None, origin_preload_data_path, processed_output_path, 
-            process_width = process_width,
-            process_height = process_height,
-            preprocess_txt_action = preprocess_txt_action,
-            process_keep_original_size = False,
-            process_flip = process_flip,
-            process_split = process_split,
-            process_caption = process_caption,
-            process_caption_deepbooru = process_caption_deepbooru,
-            split_threshold = process_split_threshold,
-            overlap_ratio = process_overlap_ratio,
-            process_focal_crop = process_focal_crop,
-            process_focal_crop_face_weight = process_focal_crop_face_weight,
-            process_focal_crop_entropy_weight = process_focal_crop_entropy_weight,
-            process_focal_crop_edges_weight = process_focal_crop_edges_weight,
-            process_focal_crop_debug = process_focal_crop_debug,
-            process_multicrop = process_multicrop,
-            process_multicrop_mindim = process_multicrop_mindim,
-            process_multicrop_maxdim = process_multicrop_maxdim,
-            process_multicrop_minarea = process_multicrop_minarea,
-            process_multicrop_maxarea = process_multicrop_maxarea,
-            process_multicrop_objective = process_multicrop_objective,
-            process_multicrop_threshold = process_multicrop_threshold,
-        )
-    except Exception as e:
-        printD("dataset update error", e)
-        print(traceback.format_exc(), file=sys.stderr)
+    
 
     return get_project_version_dataset_box_update(project, version)+[""]
 
